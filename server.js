dc5ef6d09ed473762ed15292ec2956cdb8602930
shmuel10 <shmulik.v1990@gmail.com>
2020-11-19 17:31:52 +0200

    Create page with login & sineup

---
+++ server.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', function(req,res){
+  res.render('index');
+});
+//For simcha
+app.post('/login',function(req,res) {
+  let usersArr = require("./users.json");
+  console.log(usersArr);
+  let existUser = usersArr.find(existUser => existUser.email === req.body.email );
+  if(existUser){
+    if(existUser.password === req.body.password){
+      res.status(200).send({msg : "UserExist"});
+    }else{
+      res.status(401).send({msg:"Wrong password"});
+    }
+  }else{
+    res.status(401).send({msg : "User does not exist"});
+  }
+});
+
+app.listen(8080, function () {console.log('Example app listening on port 8080!')});
