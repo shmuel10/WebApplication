e2a4a82b3c3500fd921e2200c671188a6cf4658e
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2020-11-22 11:19:51 +0200

    pusg

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ app.use(express.json());
 
 app.get('/', function (req, res) {
   res.render('index');
-  //console.log(req);
-  //let params = new URLSearchParams(req.url);
-  //console.log(params);
-  //console.log(params.get(username));
+  console.log(req);
+  let params = new URLSearchParams(req.query);
+  console.log(params.get("username"));
 });
 
 
