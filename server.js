d5f8a101a8d0a174ac16d74d36fc110a05e2c68d
shmuel10 <shmulik.v1990@gmail.com>
2020-11-22 12:59:37 +0200

    No changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 let express = require('express');
 let app = express();
+let session = require('express-session');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
+app.use(session({secret: 'dlkfjgdnbhlur4i5y38tuh',saveUninitialized: false,resave: true}));
+let usersArr = require("./data/users.json");
 
 app.get('/', function (req, res) {
   res.render('index');
@@ -12,20 +15,26 @@ app.get('/', function (req, res) {
   //console.log(params.get(username));
 });
 
-
-app.post('/login', function (req, res) {
-  let usersArr = require("./data/users.json");
-  console.log(usersArr);
-  let existUser = usersArr.find(existUser => existUser.email === req.body.email);
-  if (existUser) {
-    if (existUser.password === req.body.password) {
-      res.status(200).send({ msg: "UserExist" });
+app.post('/login', function (req, res) {  
+  if(!req.session.existUser){   
+    console.log(usersArr);
+    let existUser = usersArr.find(existUser => existUser.email === req.body.email);
+    if (existUser) {
+      if (existUser.password === req.body.password) {
+        res.status(200).send({ msg: "UserExist" });
+        req.session.existUser = existUser;
+        console.log("session user:" ,req.session.existUser);
+      } else {
+        res.status(401).send({ msg: "Wrong password" });
+      }
     } else {
-      res.status(401).send({ msg: "Wrong password" });
+      res.status(401).send({ msg: "User does not exist" });
     }
-  } else {
-    res.status(401).send({ msg: "User does not exist" });
+  } 
+  else{
+    console.log("Already logged in");
   }
 });
+
 
 app.listen(8080, function () { console.log('Example app listening on port 8080!') });
